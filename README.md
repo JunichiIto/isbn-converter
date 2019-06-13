d76ae0b85db28f24a2ba972db9d9e0ff92e3c092
Junichi Ito <jit@sonicgarden.jp>
2019-06-14 08:17:06 +0900

    Add README / Change method name

---
+++ README.md
@@ -0,0 +1,23 @@
+# ISBN Converter
+
+Convert ISBN13 to ISBN10.
+
+## Specification
+
+Please refer:
+
+[ISBN13をISBN10に変換するロジックの解説 · DQNEO起業日記](http://dqn.sakusakutto.jp/2013/10/isbn13isbn10.html)
+
+## Usage
+
+Requires Ruby 2.6.0 or higher.
+
+```ruby
+convert_isbn('9784063842760') #=> "4063842762" 
+convert_isbn('9784774183619') #=> "477418361X"
+convert_isbn('9784797386295') #=> "4797386290"
+```
+
+## License
+
+MIT License.
--- test/isbn_converter_test.rb
+++ test/isbn_converter_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 
-def change_isbn(isbn13)
+def convert_isbn(isbn13)
   (body = isbn13[3..-2])
     .each_char.with_index
     .inject(0) { |sum, (c, i)| sum + c.to_i * (10 - i) }
@@ -10,15 +10,15 @@ def change_isbn(isbn13)
 end
 
 class IsbnConverterTest < Minitest::Test
-  def test_change_isbn
-    assert_equal '4063842762', change_isbn('9784063842760')
+  def test_convert_isbn
+    assert_equal '4063842762', convert_isbn('9784063842760')
   end
 
-  def test_change_isbn_x
-    assert_equal '477418361X', change_isbn('9784774183619')
+  def test_convert_isbn_x
+    assert_equal '477418361X', convert_isbn('9784774183619')
   end
 
-  def test_change_isbn_0
-    assert_equal '4797386290', change_isbn('9784797386295')
+  def test_convert_isbn_0
+    assert_equal '4797386290', convert_isbn('9784797386295')
   end
 end
