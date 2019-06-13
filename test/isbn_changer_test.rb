c2f96bb16884be236bc0e55b00661e060811d2e9
Junichi Ito <jit@sonicgarden.jp>
2019-06-14 08:10:29 +0900

    Refactoring

--- test/isbn_changer_test.rb
+++ test/isbn_changer_test.rb
@@ -1,12 +1,12 @@
 require 'minitest/autorun'
 
 def change_isbn(isbn13)
-  isbn13[3..-2]
+  (body = isbn13[3..-2])
     .each_char.with_index
     .inject(0) { |sum, (c, i)| sum + c.to_i * (10 - i) }
     .then { |sum| 11 - sum % 11 }
     .then { |raw_digit| { 10 => 'X', 11 => 0 }[raw_digit] || raw_digit }
-    .then { |digit| "#{isbn13[3..-2]}#{digit}" }
+    .then { |digit| "#{body}#{digit}" }
 end
 
 class IsbnChangerTest < Minitest::Test
